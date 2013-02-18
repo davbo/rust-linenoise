5090a9c7c21e331b6b76d438b62222df117c0360
Dave King <dave@davbo.org>
2013-02-18 22:42:43 +0000

    Updated for rust 0.6

--- linenoise.rs
+++ linenoise.rs
@@ -7,28 +7,56 @@ pub type linenoiseCompletions = {
 extern mod linenoise {
     fn linenoise(prompt: *libc::c_char) -> *libc::c_char;
     fn linenoiseHistoryAdd(line: *libc::c_char) -> libc::c_int;
-    fn linenoiseHistorySetMaxLen(line: libc::c_int) -> libc::c_int;
-    fn linenoiseHistorySave(line: *libc::c_char) -> libc::c_int;
-    fn linenoiseHistoryLoad(line: *libc::c_char) -> libc::c_int;
+    fn linenoiseHistorySetMaxLen(length: libc::c_int) -> libc::c_int;
+    fn linenoiseHistorySave(filename: *libc::c_char) -> libc::c_int;
+    fn linenoiseHistoryLoad(filename: *libc::c_char) -> libc::c_int;
+    /*fn linenoiseSetMultiLine(enabled: libc::c_int) -> libc::c_void;*/
+    /*fn linenoiseClearScreen() -> libc::c_void;*/
 }
 
-pub fn init(prompt: &str) -> ~str unsafe {
-    let line = do str::as_c_str(prompt) |cstr| {
-        linenoise::linenoise(cstr)
-    };
+pub fn init(prompt: &str) -> ~str {
+    unsafe {
+        let line = do str::as_c_str(prompt) |cstr| {
+            linenoise::linenoise(cstr)
+        };
     str::raw::from_c_str(line)
+    }
 }
 
-pub fn history_add(line: &str) -> bool unsafe {
+pub fn history_add(line: &str) -> bool {
     let added = do str::as_c_str(line) |cstr| {
-        linenoise::linenoiseHistoryAdd(cstr)
+        unsafe {
+            linenoise::linenoiseHistoryAdd(cstr)
+        }
     };
     added as int == 1 // number of lines added perhaps?
 }
 
-pub fn history_save(filename: &str) -> bool unsafe {
+pub fn history_save(filename: &str) -> bool {
     let saved = do str::as_c_str(filename) |cstr| {
-        linenoise::linenoiseHistorySave(cstr)
+        unsafe {
+            linenoise::linenoiseHistorySave(cstr)
+        }
     };
     saved as int == 0 // Seems to always be 0 if successful?
 }
+
+/*
+   TODO: Enable once I can figure out how to include an updated version of linenoise
+   I suspect I need to build it and then provide a linkname, but not sure
+*/
+/*
+pub fn enable_multiline(enable: bool) -> bool {
+    if enable {
+        unsafe {
+            linenoise::linenoiseSetMultiLine(1);
+            true
+        }
+    } else {
+        unsafe {
+            linenoise::linenoiseSetMultiLine(0);
+            false
+        }
+    }
+}
+*/
